711756c2fa67ee2906b76ae739310103172191fe
Nullcano <mo@shi.moe>
2022-09-12 22:00:41 +0200

    prepwork

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../styles/sweet.css';
+	import '../styles/fonts.css';
+	import '../styles/global.css';
+  import AppHeader from '../lib/AppHeader.svelte';
+</script>
+
+<main>
+  <AppHeader />
+  <slot />
+</main>
